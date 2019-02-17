13eccaff7c37678581cd2011a917da43372f03b1
Tai Sakuma <tai.sakuma@gmail.com>
2019-02-17 16:53:52 +0000

    add long description= in setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,32 @@
 from setuptools import setup, find_packages
 import versioneer
 
+long_description = """
+atpbar
+------
+
+Progress bars for threading and multiprocessing tasks on terminal and
+Jupyter Notebook.
+
+The code in *atpbar* started its development in 2015 as part of
+`alphatwirl <https://github.com/alphatwirl/alphatwirl>`__. It has been a
+sub-package, *progressbar*, of alphatwirl. It became an independent
+package in February 2019. *atpbar* can display multiple progress bars
+simultaneously growing to show the progresses of iterations of loops
+in `threading <https://docs.python.org/3/library/threading.html>`__ or
+`multiprocessing <https://docs.python.org/3/library/multiprocessing.html>`__
+tasks. *atpbar* can display progress bars on terminal and `Jupyter
+Notebook <https://jupyter.org/>`__.
+
+- How to use atpbar is explained at https://github.com/alphatwirl/atpbar.
+"""
+
 setup(
     name='atpbar',
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
     description='Progress bars for threading and multiprocessing tasks',
+    long_description=long_description,
     author='Tai Sakuma',
     author_email='tai.sakuma@gmail.com',
     url='https://github.com/alphatwirl/atpbar',
