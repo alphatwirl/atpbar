bc0a79adf13be1ba333d273802d932c597ff5b5d
Tai Sakuma <tai.sakuma@gmail.com>
2019-02-19 08:22:30 +0000

    add test_is_jupyter_notebook()

---
+++ tests/presentation/test_is_jupyter_notebook.py
@@ -0,0 +1,9 @@
+# Tai Sakuma <tai.sakuma@gmail.com>
+
+from atpbar.presentation.create import is_jupyter_notebook
+
+##__________________________________________________________________||
+def test_is_jupyter_notebook():
+    is_jupyter_notebook()
+
+##__________________________________________________________________||
