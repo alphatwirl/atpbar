74b03b8422ae461eff75137688e7bea1853595ce
Tai Sakuma <tai.sakuma@gmail.com>
2019-02-19 04:32:33 +0000

    update test for ProgressReport

--- tests/test_ProgressReport.py
+++ tests/test_ProgressReport.py
@@ -1,37 +1,36 @@
 # Tai Sakuma <tai.sakuma@gmail.com>
+import pytest
 
 from atpbar.report import ProgressReport
 
 ##__________________________________________________________________||
 def test_repr():
-    obj = ProgressReport(name = 'dataset1', done = 124, total = 1552, taskid = 1212)
+    obj = ProgressReport(name='dataset1', done=124, total=1552, taskid=1212)
     repr(obj)
 
 def test_init():
-    obj = ProgressReport(name = 'dataset1', done = 124, total = 1552, taskid = 1212)
+    obj = ProgressReport(name='dataset1', done=124, total=1552, taskid=1212)
 
 def test_init_no_taskid():
-    obj = ProgressReport(name = 'dataset1', done = 124, total = 1552)
+    obj = ProgressReport(name='dataset1', done=124, total=1552)
     assert obj.taskid == 'dataset1'
 
-def test_last():
-    obj = ProgressReport(name = 'dataset1', done = 124, total = 1552)
-    assert not obj.last()
+params = [
+    (ProgressReport(name='dataset1', done=124, total=1552), False),
+    (ProgressReport(name='dataset1', done=1552, total=1552), True),
+    (ProgressReport(name='dataset1', done=0, total=0), True),
+]
+@pytest.mark.parametrize('obj, expected', params)
+def test_last(obj, expected):
+    assert expected == obj.last()
 
-    obj = ProgressReport(name = 'dataset1', done = 1552, total = 1552)
-    assert obj.last()
-
-    obj = ProgressReport(name = 'dataset1', done = 0, total = 0)
-    assert obj.last()
-
-def test_first():
-    obj = ProgressReport(name = 'dataset1', done = 124, total = 1552)
-    assert not obj.last()
-
-    obj = ProgressReport(name = 'dataset1', done = 0, total = 1552)
-    assert obj.first()
-
-    obj = ProgressReport(name = 'dataset1', done = 0, total = 0)
-    assert obj.first()
+params = [
+    (ProgressReport(name='dataset1', done=124, total=1552), False),
+    (ProgressReport(name='dataset1', done=0, total=1552), True),
+    (ProgressReport(name='dataset1', done=0, total=0), True),
+]
+@pytest.mark.parametrize('obj, expected', params)
+def test_first(obj, expected):
+    assert expected == obj.first()
 
 ##__________________________________________________________________||
