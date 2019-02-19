ac5e3102d2a2dc373b4656927599cf53c8d2e990
Tai Sakuma <tai.sakuma@gmail.com>
2019-02-19 19:17:04 +0000

    modified _queue in the pytest.fixture global_variables()

--- tests/scenarios/conftest.py
+++ tests/scenarios/conftest.py
@@ -35,6 +35,7 @@ def global_variables(monkeypatch):
     monkeypatch.setattr(module, '_presentation', None)
     monkeypatch.setattr(module, '_reporter', None)
     monkeypatch.setattr(module, '_pickup', None)
+    monkeypatch.setattr(module, '_queue', None)
     monkeypatch.setattr(module, '_lock', threading.Lock())
     yield
     flush()
