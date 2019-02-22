142f39c0be5b7639878fcfd35635126fffb7e35e
Tai Sakuma <tai.sakuma@gmail.com>
2019-02-22 08:17:41 +0000

    add ProgressReportComplementer

--- atpbar/report.py
+++ atpbar/report.py
@@ -1,34 +1,37 @@
 # Tai Sakuma <tai.sakuma@gmail.com>
-import os
 
 ##__________________________________________________________________||
 class ProgressReport(object):
     """A progress report
 
+    The `taskid` is mandatory. The other fields will be complemented
+    with the previous report for the same `taskid`.
+
     Parameters
     ----------
-    name : str
-        A name of the task. It will be use as the label on the
-        progress bars.
-    done : int
-        The number of the iterations done so far
-    total : int
-        The total iterations to be done
     taskid : immutable
         The unique task ID.
-    pid :
+    name : str, optional
+        A name of the task. It will be use as the label on the
+        progress bars.
+    done : int, optional
+        The number of the iterations done so far
+    total : int, optional
+        The total iterations to be done
+    pid : optional
         The process ID, e.g., the value returned by `os.getpid()`
-    in_main_thread : bool
+    in_main_thread : bool, optional
         True if the task is running in the main thread of the process.
         False otherwise
 
     """
 
-    def __init__(self, name, done, total, taskid, pid, in_main_thread):
+    def __init__(self, taskid, done=None, total=None, name=None, pid=None,
+                 in_main_thread=None):
+        self.taskid = taskid
         self.name = name
         self.done = done
         self.total = total
-        self.taskid = taskid
         self.pid = pid
         self.in_main_thread = in_main_thread
 
@@ -51,5 +54,46 @@ class ProgressReport(object):
 
     def first(self):
          return self.done == 0
+
+##__________________________________________________________________||
+class ProgressReportComplementer(object):
+    """Complement progress reports
+
+    Complement a progress report with the previous report for the same
+    task.
+
+    """
+    def __init__(self):
+        self.previous_reports = { }
+        self.volatile_fileds = ('first', 'last')
+
+    def __call__(self, report):
+        taskid = report['taskid']
+        if taskid in self.previous_reports:
+            self._complement(taskid, report)
+        self._first(report)
+        self._last(report)
+        self._store(taskid, report.copy())
+
+    def _complement(self, taskid, report):
+        report_copy = report.copy()
+        report.clear()
+        report.update(self.previous_reports[taskid])
+        report.update(report_copy)
+
+    def _first(self, report):
+        if 'first' in report:
+            return
+        report['first'] = (report['done'] == 0)
+
+    def _last(self, report):
+        if 'last' in report:
+            return
+        report['last'] = (report['done'] >= report['total'])
+
+    def _store(self, taskid, report):
+        for k in self.volatile_fileds:
+            report.pop(k, None)
+        self.previous_reports[taskid] = report
 
 ##__________________________________________________________________||
---
+++ tests/test_report_complement.py
@@ -0,0 +1,121 @@
+# Tai Sakuma <tai.sakuma@gmail.com>
+import pytest
+
+from atpbar.report import ProgressReport, ProgressReportComplementer
+
+##__________________________________________________________________||
+@pytest.fixture()
+def obj():
+    ret = ProgressReportComplementer()
+    return ret
+
+def test_repr(obj):
+    repr(obj)
+
+##__________________________________________________________________||
+def test_complement(obj):
+
+    report0 = dict(taskid=5355, done=0, total=10, name='task1', pid=2342, in_main_thread=True)
+    expected0 = dict(taskid=5355, done=0, total=10, first=True, last=False, name='task1', pid=2342, in_main_thread=True)
+    assert obj(report0) is None
+    assert expected0 == report0
+
+    report1 = dict(taskid=5355, done=1, total=12)
+    expected1 = dict(taskid=5355, done=1, total=12, first=False, last=False, name='task1', pid=2342, in_main_thread=True)
+    assert obj(report1) is None
+    assert expected0 == report0
+    assert expected1 == report1
+
+    report2 = dict(taskid=5355, done=2)
+    expected2 = dict(taskid=5355, done=2, total=12, first=False, last=False, name='task1', pid=2342, in_main_thread=True)
+    assert obj(report2) is None
+    assert expected0 == report0
+    assert expected1 == report1
+    assert expected2 == report2
+
+    report3 = dict(taskid=5355, done=12)
+    expected3 = dict(taskid=5355, done=12, total=12, first=False, last=True, name='task1', pid=2342, in_main_thread=True)
+    assert obj(report3) is None
+    assert expected0 == report0
+    assert expected1 == report1
+    assert expected2 == report2
+    assert expected3 == report3
+
+##__________________________________________________________________||
+def test_volatile_fileds(obj):
+
+    report0 = dict(taskid=5355, done=0, total=10, first=True)
+    expected0 = dict(taskid=5355, done=0, total=10, first=True, last=False)
+    assert obj(report0) is None
+    assert expected0 == report0
+
+    # manually set `first`
+    report1 = dict(taskid=5355, done=1, total=12, first=True, last=False)
+    expected1 = dict(taskid=5355, done=1, total=12, first=True, last=False)
+    assert obj(report1) is None
+    assert expected0 == report0
+    assert expected1 == report1
+
+    # doesn't remember previously manually set `first`
+    report2 = dict(taskid=5355, done=2)
+    expected2 = dict(taskid=5355, done=2, total=12, first=False, last=False)
+    assert obj(report2) is None
+    assert expected0 == report0
+    assert expected1 == report1
+    assert expected2 == report2
+
+##__________________________________________________________________||
+def test_first(obj):
+
+    report0 = dict(taskid=5355, done=0, total=10)
+    expected0 = dict(taskid=5355, done=0, total=10, first=True, last=False)
+    assert obj(report0) is None
+    assert expected0 == report0
+
+    report1 = dict(taskid=5355, done=1, total=12)
+    expected1 = dict(taskid=5355, done=1, total=12, first=False, last=False)
+    assert obj(report1) is None
+    assert expected1 == report1
+
+    report2 = dict(taskid=5355, done=2, first=True)
+    expected2 = dict(taskid=5355, done=2, total=12, first=True, last=False)
+    assert obj(report2) is None
+    assert expected2 == report2
+
+    report3 = dict(taskid=5355, done=3)
+    expected3 = dict(taskid=5355, done=3, total=12, first=False, last=False)
+    assert obj(report3) is None
+    assert expected3 == report3
+
+    # different taskid, both done and total are 0
+    report4 = dict(taskid=9222, done=0, total=0)
+    expected4 = dict(taskid=9222, done=0, total=0, first=True, last=True)
+    assert obj(report4) is None
+    assert expected4 == report4
+
+##__________________________________________________________________||
+def test_last(obj):
+
+    report0 = dict(taskid=5355, done=0, total=10)
+    expected0 = dict(taskid=5355, done=0, total=10, first=True, last=False)
+    assert obj(report0) is None
+    assert expected0 == report0
+
+    # set `last` manually
+    report1 = dict(taskid=5355, done=1, total=12, last=True)
+    expected1 = dict(taskid=5355, done=1, total=12, first=False, last=True)
+    assert obj(report1) is None
+    assert expected1 == report1
+
+    report2 = dict(taskid=9222, done=0, total=10)
+    expected2 = dict(taskid=9222, done=0, total=10, first=True, last=False)
+    assert obj(report2) is None
+    assert expected2 == report2
+
+    # done = total
+    report3 = dict(taskid=9222, done=10, total=10)
+    expected3 = dict(taskid=9222, done=10, total=10, first=False, last=True)
+    assert obj(report3) is None
+    assert expected3 == report3
+
+##__________________________________________________________________||
