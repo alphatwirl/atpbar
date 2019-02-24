9f2706dfe5fec144f1536305794c0db07a176772
Tai Sakuma <tai.sakuma@gmail.com>
2019-02-24 16:01:23 +0000

    reorder tests in test_multiprocessing.py
    for easier debugging at travis-ci

--- tests/scenarios/test_multiprocessing.py
+++ tests/scenarios/test_multiprocessing.py
@@ -76,35 +76,35 @@ def test_multiprocessing_from_loop(
         assert nreports_expected == len(progressbar0.reports)
         # one report from `atpbar` in the main thread
 
-        assert 1 == len(progressbar0.taskids)
         assert 1 == progressbar0.nfirsts
         assert 1 == progressbar0.nlasts
+        assert 1 == len(progressbar0.taskids)
 
     else:
         if 2 == len(presentations):
 
+            progressbar1 = presentations[1]
+            assert 0 == len(progressbar1.reports)
+
             progressbar0 = presentations[0]
-            assert nreports_expected == len(progressbar0.reports)
             assert ntasks + 1 == len(progressbar0.taskids)
             assert ntasks + 1 == progressbar0.nfirsts
             assert ntasks + 1 == progressbar0.nlasts
-
-            progressbar1 = presentations[1]
-            assert 0 == len(progressbar1.reports)
+            assert nreports_expected == len(progressbar0.reports)
 
         else:
             assert 3 == len(presentations)
 
+            progressbar2 = presentations[2]
+            assert 0 == len(progressbar2.reports)
+
             progressbar0 = presentations[0]
             progressbar1 = presentations[1]
 
-            assert nreports_expected == len(progressbar0.reports) + len(progressbar1.reports)
             assert ntasks + 1 == len(progressbar0.taskids) + len(progressbar1.taskids)
             assert ntasks + 1 == progressbar0.nfirsts + progressbar1.nfirsts
             assert ntasks + 1 == progressbar0.nlasts + progressbar1.nlasts
-
-            progressbar2 = presentations[2]
-            assert 0 == len(progressbar2.reports)
+            assert nreports_expected == len(progressbar0.reports) + len(progressbar1.reports)
 
     # At this point the pickup shouldn't be owned. Therefore, a new
     # `atpbar` in the main thread should own it.
@@ -113,9 +113,9 @@ def test_multiprocessing_from_loop(
         pass
     assert npresentations + 1 == len(presentations)
     progressbar = presentations[-2]
-    assert 4 + 1 == len(progressbar.reports)
     assert 1 == len(progressbar.taskids)
     assert 1 == progressbar.nfirsts
     assert 1 == progressbar.nlasts
+    assert 4 + 1 == len(progressbar.reports)
 
 ##__________________________________________________________________||
