9e151d7cf45c52d15476e02656ba947746108cea
Tai Sakuma <tai.sakuma@gmail.com>
2019-04-23 14:50:35 +0200

    make the name filed longer depending on the terminal width

--- atpbar/presentation/bartty.py
+++ atpbar/presentation/bartty.py
@@ -1,7 +1,11 @@
 # Tai Sakuma <tai.sakuma@gmail.com>
+import os
 import sys
 
 from .base import Presentation
+
+##__________________________________________________________________||
+MINIMUM_TERMINAL_WIDTH = 90
 
 ##__________________________________________________________________||
 class ProgressBar(Presentation):
@@ -9,11 +13,28 @@ class ProgressBar(Presentation):
         super(ProgressBar, self).__init__()
         self.lines = [ ]
         self.interval = 0.1 # [second]
+        self.width = self._get_width()
 
     def __repr__(self):
         return '{}()'.format(
             self.__class__.__name__
         )
+
+    def _get_width(self):
+        try:
+            import shutil
+            columns = shutil.get_terminal_size().columns
+            return max(MINIMUM_TERMINAL_WIDTH, columns - 1)
+        except AttributeError:
+            # Python 2
+            pass
+
+        try:
+            rows, columns = os.popen('stty size', 'r').read().split()
+            columns = int(columns)
+            return max(MINIMUM_TERMINAL_WIDTH, columns - 1)
+        except Exception:
+            return MINIMUM_TERMINAL_WIDTH
 
     def _present(self):
         self._delete_previous_lines()
@@ -39,16 +60,15 @@ class ProgressBar(Presentation):
             self.last.append(line)
 
     def _create_line(self, report):
-        name_field_length = 32
         percent = float(report['done'])/report['total'] if report['total'] > 0 else 1
         bar = (':' * int(percent * 40)).ljust(40, " ")
         percent = round(percent * 100, 2)
-        name = report['name'][0:name_field_length]
-        format = ' {percent:6.2f}% {bar:s} | {done:8d} / {total:8d} |:  {name:<{name_field_length}s} '
+        format = ' {percent:6.2f}% {bar:s} | {done:8d} / {total:8d} |:  {name} '
         ret = format.format(
             percent=percent, bar=bar,
             done=report['done'], total=report['total'],
-            name=name, name_field_length=name_field_length)
+            name=report['name'])
+        ret = ret[:self.width].ljust(self.width, ' ')
         return ret
 
     def _print_lines(self):
