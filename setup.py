08683dcaab22f2f03890acb2c25299685d7f63e7
Tai Sakuma <tai.sakuma@gmail.com>
2020-02-15 09:44:40 -0500

    update setup.py, adding Python 3.8

--- setup.py
+++ setup.py
@@ -26,6 +26,7 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
     packages=find_packages(exclude=['docs', 'tests'])
 )
