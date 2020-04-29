b2777552f1403af68c456ae27de7123dd0f42020
Tai Sakuma <tai.sakuma@gmail.com>
2020-04-28 21:50:17 -0400

    make detach_pickup() a method of Started

--- atpbar/machine.py
+++ atpbar/machine.py
@@ -88,7 +88,7 @@ class Started(State):
 
     def _start_pickup(self):
         presentation = create_presentation()
-        self.pickup = ProgressReportPickup(self.queue, presentation, detach.detach_pickup)
+        self.pickup = ProgressReportPickup(self.queue, presentation, self.detach_pickup)
         self.pickup.start()
 
     def _end_pickup(self):
@@ -121,6 +121,28 @@ class Started(State):
                 return
             with self.machine.lock:
                 self._restart_pickup()
+
+    def detach_pickup(self):
+        """detach the pickup from any instances of `atpbar`
+
+        If a pickup is started by an `atpbar` in the main thread of the
+        main process, the pickup will be owned by the `atpbar`. The
+        `atpbar` will end the pickup when the loop ends. This function
+        detaches the pickup from the `atpbar` and prevents the atpbar from
+        ending the pickup.
+
+        This function is typically called by the pickup when the pickup
+        receives a report from a sub-thread or a sub-process.
+
+        """
+
+        detach.to_detach_pickup = True
+         # Locking here would causes a deadlock. `flush()` locks while
+         # ending the pickup. Before receiving the end order, the pickup
+         # might still receives a report with a new task ID from a
+         # sub-thread or sub-process, it will call this function and will
+         # cause a deadlock.
+
 
     def flush(self):
         self._restart_pickup()
