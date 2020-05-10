a7d12123b6cd6fad703b75bacc8a56a0ebaba52b
Tai Sakuma <tai.sakuma@gmail.com>
2020-05-10 15:58:15 -0400

    add StreamRedirection

--- atpbar/stream.py
+++ atpbar/stream.py
@@ -4,6 +4,41 @@ from enum import Enum
 import threading
 
 ##__________________________________________________________________||
+class StreamRedirection:
+    def __init__(self, queue, presentation):
+        self.disabled = not presentation.stdout_stderr_redrection
+        if self.disabled:
+            return
+
+        self.queue = queue
+        self.presentation = presentation
+
+        self.stdout = Stream(self.queue, FD.STDOUT)
+        self.stderr = Stream(self.queue, FD.STDERR)
+
+    def start(self):
+        if self.disabled:
+            return
+
+        self.pickup = StreamPickup(self.queue, self.presentation.stdout_write, self.presentation.stderr_write)
+        self.pickup.start()
+
+        self.stdout_org = sys.stdout
+        sys.stdout = self.stdout
+
+        self.stderr_org = sys.stderr
+        sys.stderr = self.stderr
+
+    def end(self):
+        if self.disabled:
+            return
+
+        sys.stdout = self.stdout_org
+        sys.stderr = self.stderr_org
+        self.queue.put(None)
+        self.pickup.join()
+
+##__________________________________________________________________||
 class FD(Enum):
     STDOUT = 1
     STDERR = 2
