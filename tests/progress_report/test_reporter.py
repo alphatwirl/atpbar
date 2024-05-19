15e41f30bd2a2ec67eb0e39c58ce0af0bd8860fb
Tai Sakuma <tai.sakuma@gmail.com>
2024-05-19 17:57:20 -0400

    Correct types

--- tests/progress_report/test_reporter.py
+++ tests/progress_report/test_reporter.py
@@ -1,19 +1,20 @@
 import sys
+import unittest.mock as mock
+import uuid
+from typing import Any
 
 import pytest
 
-import unittest.mock as mock
-
-from atpbar.progress_report.reporter import ProgressReporter
+from atpbar.progress_report import ProgressReporter, Report
 
 
 @pytest.fixture()
-def mock_queue():
+def mock_queue() -> mock.Mock:
     return mock.Mock()
 
 
 @pytest.fixture()
-def mock_time(monkeypatch):
+def mock_time(monkeypatch: pytest.MonkeyPatch) -> mock.Mock:
     ret = mock.Mock()
     module = sys.modules["atpbar.progress_report.reporter"]
     monkeypatch.setattr(module, "time", ret)
@@ -21,53 +22,79 @@ def mock_time(monkeypatch):
 
 
 @pytest.fixture()
-def obj(mock_queue, mock_time):
+def obj(mock_queue: mock.Mock, mock_time: mock.Mock) -> ProgressReporter:
     ret = ProgressReporter(
         mock_queue, notices_from_sub_processes=mock.Mock(), stream_queue=mock.Mock()
     )
     return ret
 
 
-def test_repr(obj):
+def test_repr(obj: ProgressReporter) -> None:
     repr(obj)
 
 
 class TestReport:
 
-    def test_report_need_to_report(self, obj, monkeypatch, mock_queue, mock_time):
+    def test_report_need_to_report(
+        self,
+        obj: ProgressReporter,
+        monkeypatch: pytest.MonkeyPatch,
+        mock_queue: mock.Mock,
+        mock_time: mock.Mock,
+    ) -> None:
         current_time = 15324.345
-        taskid = 234
+        taskid = uuid.uuid4()
         mock_time.time.return_value = current_time
         monkeypatch.setattr(obj, "_need_to_report", mock.Mock(return_value=True))
-        report = dict(taskid=taskid, done=0, total=10)
+        report = Report(taskid=taskid, done=0, total=10)
         obj.report(report)
         assert [mock.call(report)] == mock_queue.put.call_args_list
         assert {taskid: current_time} == obj.last_time
 
-    def test_report_no_need_to_report(self, obj, monkeypatch, mock_queue, mock_time):
+    def test_report_no_need_to_report(
+        self,
+        obj: ProgressReporter,
+        monkeypatch: pytest.MonkeyPatch,
+        mock_queue: mock.Mock,
+        mock_time: mock.Mock,
+    ) -> None:
         current_time = 15324.345
-        taskid = 234
+        taskid = uuid.uuid4()
         mock_time.time.return_value = current_time
         monkeypatch.setattr(obj, "_need_to_report", mock.Mock(return_value=False))
-        report = dict(taskid=taskid, done=0, total=10)
+        report = Report(taskid=taskid, done=0, total=10)
         obj.report(report)
         assert [] == mock_queue.put.call_args_list
         assert {} == obj.last_time
 
 
+task_id = uuid.uuid4()
+
 params = [
-    pytest.param(dict(taskid=1, first=True, last=False), {}, 10.0, True),
-    pytest.param(dict(taskid=1, first=False, last=True), {}, 10.0, True),
-    pytest.param(dict(taskid=1, first=True, last=False), {1: 10.0}, 10.0, True),
-    pytest.param(dict(taskid=1, first=False, last=False), {1: 10.0}, 30.0, True),
-    pytest.param(dict(taskid=1, first=False, last=False), {1: 10.0}, 15.0, False),
+    pytest.param(Report(taskid=task_id, first=True, last=False), {}, 10.0, True),
+    pytest.param(Report(taskid=task_id, first=False, last=True), {}, 10.0, True),
+    pytest.param(
+        Report(taskid=task_id, first=True, last=False), {task_id: 10.0}, 10.0, True
+    ),
+    pytest.param(
+        Report(taskid=task_id, first=False, last=False), {task_id: 10.0}, 30.0, True
+    ),
+    pytest.param(
+        Report(taskid=task_id, first=False, last=False), {task_id: 10.0}, 15.0, False
+    ),
 ]
-param_names = "report, last_time, current_time, " "expected"
+param_names = 'report, last_time, current_time, expected'
 
 
 @pytest.mark.parametrize(param_names, params)
-def test_need_to_report(obj, mock_time, report, last_time, current_time, expected):
-
+def test_need_to_report(
+    obj: ProgressReporter,
+    mock_time: mock.Mock,
+    report: Report,
+    last_time: dict[uuid.UUID, float],
+    current_time: Any,
+    expected: bool,
+) -> None:
     obj.interval = 10
     obj.last_time = last_time
     mock_time.time.return_value = current_time
