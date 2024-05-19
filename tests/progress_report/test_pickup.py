b0fb52bc7b91d72d03917bb6fbf4a4dde01a4859
Tai Sakuma <tai.sakuma@gmail.com>
2024-05-19 18:12:04 -0400

    Correct types

--- tests/progress_report/test_pickup.py
+++ tests/progress_report/test_pickup.py
@@ -1,6 +1,6 @@
 import multiprocessing
-import time
 import unittest.mock as mock
+from collections.abc import Iterator
 
 import pytest
 
@@ -10,22 +10,34 @@ from atpbar.progress_report.pickup import ProgressReportPickup
 class TestStart:
 
     @pytest.fixture()
-    def presentation(self):
+    def presentation(self) -> mock.Mock:
         return mock.Mock()
 
     @pytest.fixture()
-    def queue(self):
+    def queue(self) -> multiprocessing.Queue:
         return multiprocessing.Queue()
 
     @pytest.fixture()
-    def pickup(self, queue, presentation):
+    def pickup(
+        self, queue: multiprocessing.Queue, presentation: mock.Mock
+    ) -> ProgressReportPickup:
         presentation.active.return_value = True
         return ProgressReportPickup(queue, presentation)
 
-    def test_end(self, pickup, queue, presentation):
+    def test_end(
+        self,
+        pickup: ProgressReportPickup,
+        queue: multiprocessing.Queue,
+        presentation: mock.Mock,
+    ) -> None:
         pickup.end()
 
-    def test_daemon(self, pickup, queue, presentation):
+    def test_daemon(
+        self,
+        pickup: ProgressReportPickup,
+        queue: multiprocessing.Queue,
+        presentation: mock.Mock,
+    ) -> None:
         pass
         # it is ok not to execute `pickup.end()`.
 
@@ -33,47 +45,71 @@ class TestStart:
 class TestRunUntilTheEndOrderArrives:
 
     @pytest.fixture()
-    def mock_thread_start(self, monkeypatch):
+    def mock_thread_start(self, monkeypatch: pytest.MonkeyPatch) -> Iterator[mock.Mock]:
         y = mock.Mock()
-        monkeypatch.setattr(ProgressReportPickup, "start", y)
+        monkeypatch.setattr(ProgressReportPickup, 'start', y)
         yield y
 
     @pytest.fixture()
-    def mock_queue(self):
+    def mock_queue(self) -> mock.Mock:
         return mock.Mock()
 
     @pytest.fixture()
-    def presentation(self):
+    def presentation(self) -> mock.Mock:
         return mock.Mock()
 
     @pytest.fixture()
-    def pickup(self, mock_queue, presentation, mock_thread_start):
+    def pickup(
+        self,
+        mock_queue: mock.Mock,
+        presentation: mock.Mock,
+        mock_thread_start: mock.Mock,
+    ) -> Iterator[ProgressReportPickup]:
         y = ProgressReportPickup(mock_queue, presentation)
         yield y
 
     @pytest.fixture(autouse=True)
-    def mock_short_sleep(self, pickup, monkeypatch):
+    def mock_short_sleep(
+        self,
+        pickup: ProgressReportPickup,
+        monkeypatch: pytest.MonkeyPatch,
+    ) -> Iterator[mock.Mock]:
         y = mock.Mock()
-        monkeypatch.setattr(pickup, "_short_sleep", y)
+        monkeypatch.setattr(pickup, '_short_sleep', y)
         yield y
 
-    def test_no_report(self, pickup, mock_queue, presentation):
+    def test_no_report(
+        self,
+        pickup: ProgressReportPickup,
+        mock_queue: mock.Mock,
+        presentation: mock.Mock,
+    ) -> None:
         mock_queue.empty.side_effect = [False, True]
         mock_queue.get.side_effect = [None]
         pickup._run_until_the_end_order_arrives()
         assert [] == presentation.mock_calls
         assert [mock.call(), mock.call()] == mock_queue.empty.mock_calls
-        assert 0 == pickup._short_sleep.call_count
+        assert 0 == pickup._short_sleep.call_count  # type: ignore
 
-    def test_one_report(self, pickup, mock_queue, presentation):
+    def test_one_report(
+        self,
+        pickup: ProgressReportPickup,
+        mock_queue: mock.Mock,
+        presentation: mock.Mock,
+    ) -> None:
         report = mock.MagicMock()
         mock_queue.empty.side_effect = [False, False, True]
         mock_queue.get.side_effect = [report, None]
         pickup._run_until_the_end_order_arrives()
         assert [mock.call.present(report)] == presentation.mock_calls
-        assert 0 == pickup._short_sleep.call_count
+        assert 0 == pickup._short_sleep.call_count  # type: ignore
 
-    def test_one_report_once_empty(self, pickup, mock_queue, presentation):
+    def test_one_report_once_empty(
+        self,
+        pickup: ProgressReportPickup,
+        mock_queue: mock.Mock,
+        presentation: mock.Mock,
+    ) -> None:
         report1 = mock.MagicMock()
         mock_queue.empty.side_effect = [
             False,
@@ -84,9 +120,14 @@ class TestRunUntilTheEndOrderArrives:
         mock_queue.get.side_effect = [report1, None]
         pickup._run_until_the_end_order_arrives()
         assert [mock.call.present(report1)] == presentation.mock_calls
-        assert 1 == pickup._short_sleep.call_count
+        assert 1 == pickup._short_sleep.call_count  # type: ignore
 
-    def test_two_reports(self, pickup, mock_queue, presentation):
+    def test_two_reports(
+        self,
+        pickup: ProgressReportPickup,
+        mock_queue: mock.Mock,
+        presentation: mock.Mock,
+    ) -> None:
         report1 = mock.MagicMock()
         report2 = mock.MagicMock()
         mock_queue.empty.side_effect = [False, False, False, True]
@@ -97,37 +138,46 @@ class TestRunUntilTheEndOrderArrives:
             mock.call.present(report1),
             mock.call.present(report2),
         ] == presentation.mock_calls
-        assert 0 == pickup._short_sleep.call_count
+        assert 0 == pickup._short_sleep.call_count  # type: ignore
 
 
 class TestRunUntilReportsStopComing:
 
     @pytest.fixture()
-    def mock_thread_start(self, monkeypatch):
+    def mock_thread_start(self, monkeypatch: pytest.MonkeyPatch) -> Iterator[mock.Mock]:
         y = mock.Mock()
         monkeypatch.setattr(ProgressReportPickup, "start", y)
         yield y
 
     @pytest.fixture()
-    def mock_queue(self):
+    def mock_queue(self) -> mock.Mock:
         return mock.Mock()
 
     @pytest.fixture()
-    def presentation(self):
+    def presentation(self) -> mock.Mock:
         return mock.Mock()
 
     @pytest.fixture()
-    def pickup(self, mock_queue, presentation, mock_thread_start):
+    def pickup(
+        self,
+        mock_queue: mock.Mock,
+        presentation: mock.Mock,
+        mock_thread_start: mock.Mock,
+    ) -> ProgressReportPickup:
         return ProgressReportPickup(mock_queue, presentation)
 
     @pytest.fixture(autouse=True)
-    def mock_short_sleep(self, pickup, monkeypatch):
+    def mock_short_sleep(
+        self,
+        pickup: ProgressReportPickup,
+        monkeypatch: pytest.MonkeyPatch,
+    ) -> Iterator[mock.Mock]:
         y = mock.Mock()
         monkeypatch.setattr(pickup, "_short_sleep", y)
         yield y
 
     @pytest.fixture(autouse=True)
-    def mock_time(self, monkeypatch):
+    def mock_time(self, monkeypatch: pytest.MonkeyPatch) -> Iterator[mock.Mock]:
         ret = mock.Mock()
         ret.time.return_value = 1000.0
         from atpbar.progress_report import pickup as m
@@ -135,22 +185,39 @@ class TestRunUntilReportsStopComing:
         monkeypatch.setattr(m, "time", ret)
         return ret
 
-    def test_no_report(self, pickup, mock_time, presentation):
+    def test_no_report(
+        self,
+        pickup: ProgressReportPickup,
+        mock_time: mock.Mock,
+        presentation: mock.Mock,
+    ) -> None:
         presentation.active.side_effect = [False]
         pickup._run_until_reports_stop_coming()
         assert [] == presentation.present.mock_calls
-        assert 0 == pickup._short_sleep.call_count
+        assert 0 == pickup._short_sleep.call_count  # type: ignore
 
-    def test_one_report(self, pickup, mock_time, mock_queue, presentation):
+    def test_one_report(
+        self,
+        pickup: ProgressReportPickup,
+        mock_time: mock.Mock,
+        mock_queue: mock.Mock,
+        presentation: mock.Mock,
+    ) -> None:
         presentation.active.side_effect = [True, False]
         report = mock.MagicMock()
         mock_queue.empty.side_effect = [False, False, True]
         mock_queue.get.side_effect = [report, None]
         pickup._run_until_reports_stop_coming()
         assert [mock.call(report)] == presentation.present.mock_calls
-        assert 1 == pickup._short_sleep.call_count
+        assert 1 == pickup._short_sleep.call_count  # type: ignore
 
-    def test_one_report_timeout(self, pickup, mock_time, mock_queue, presentation):
+    def test_one_report_timeout(
+        self,
+        pickup: ProgressReportPickup,
+        mock_time: mock.Mock,
+        mock_queue: mock.Mock,
+        presentation: mock.Mock,
+    ) -> None:
         presentation.active.return_value = True
         report = mock.MagicMock()
         mock_queue.empty.return_value = True
@@ -158,4 +225,4 @@ class TestRunUntilReportsStopComing:
         mock_time.time.side_effect = [1000.0, 1000.2, 1003.0]
         pickup._run_until_reports_stop_coming()
         assert [] == presentation.present.mock_calls
-        assert 1 == pickup._short_sleep.call_count
+        assert 1 == pickup._short_sleep.call_count  # type: ignore
