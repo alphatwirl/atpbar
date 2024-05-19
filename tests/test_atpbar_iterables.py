356715feea645d111daaedcb424474cacd605898
Tai Sakuma <tai.sakuma@gmail.com>
2024-05-19 19:14:17 -0400

    Add type hints

--- tests/test_atpbar_iterables.py
+++ tests/test_atpbar_iterables.py
@@ -1,18 +1,24 @@
 import unittest.mock as mock
+from collections.abc import Iterator, Sequence
+from typing import Any, Generic, TypeVar
 
 import pytest
 
 import atpbar
 
+T = TypeVar('T')
+
 
 @pytest.fixture()
-def mock_reporter(monkeypatch):
+def mock_reporter(monkeypatch: pytest.MonkeyPatch) -> mock.Mock:
     ret = mock.Mock()
     return ret
 
 
 @pytest.fixture(autouse=True)
-def mock_fetch_reporter(monkeypatch, mock_reporter):
+def mock_fetch_reporter(
+    monkeypatch: pytest.MonkeyPatch, mock_reporter: mock.Mock
+) -> mock.Mock:
     ret = mock.Mock()
     ret.return_value.__enter__ = mock.Mock()
     ret.return_value.__enter__.return_value = mock_reporter
@@ -21,37 +27,39 @@ def mock_fetch_reporter(monkeypatch, mock_reporter):
     return ret
 
 
-def test_mock_fetch_reporter(mock_fetch_reporter, mock_reporter):
+def test_mock_fetch_reporter(
+    mock_fetch_reporter: mock.Mock, mock_reporter: mock.Mock
+) -> None:
     with mock_fetch_reporter() as reporter:
         assert reporter is mock_reporter
 
 
-class Iter:
-    def __init__(self, content):
+class Iter(Generic[T]):
+    def __init__(self, content: Sequence[T]) -> None:
         self.content = content
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self.content)
 
-    def __iter__(self):
+    def __iter__(self) -> Iterator[T]:
         for e in self.content:
             yield e
 
 
-class GetItem:
-    def __init__(self, content):
+class GetItem(Generic[T]):
+    def __init__(self, content: Sequence[T]) -> None:
         self.content = content
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self.content)
 
-    def __getitem__(self, i):
+    def __getitem__(self, i: int) -> T:
         return self.content[i]
 
 
-iterable_classes = [list, Iter, GetItem]
+iterable_classes = list[type[Any]]([list, Iter, GetItem])
 
-empty = []
+empty = list[Any]()
 one = [mock.sentinel.item1]
 three = [mock.sentinel.item1, mock.sentinel.item2, mock.sentinel.item3]
 contents = [empty, one, three]
@@ -60,14 +68,16 @@ contents_ids = ["empty", "one", "three"]
 
 @pytest.mark.parametrize("content", contents, ids=contents_ids)
 @pytest.mark.parametrize("iterable_class", iterable_classes)
-def test_iterable(iterable_class, content):
+def test_iterable(iterable_class: type[Any], content: list[Any]) -> None:
     iterable = iterable_class(content)
     assert content == [e for e in iterable]
 
 
 @pytest.mark.parametrize("content", contents, ids=contents_ids)
 @pytest.mark.parametrize("iterable_class", iterable_classes)
-def test_atpbar_iterables(mock_reporter, iterable_class, content):
+def test_atpbar_iterables(
+    mock_reporter: mock.Mock, iterable_class: type[Any], content: list[Any]
+) -> None:
     iterable = iterable_class(content)
 
     ##
@@ -97,7 +107,9 @@ def test_atpbar_iterables(mock_reporter, iterable_class, content):
 
 @pytest.mark.parametrize("content", contents, ids=contents_ids)
 @pytest.mark.parametrize("iterable_class", iterable_classes)
-def test_atpbar_enumerate(mock_reporter, iterable_class, content):
+def test_atpbar_enumerate(
+    mock_reporter: mock.Mock, iterable_class: type[Any], content: list[Any]
+) -> None:
     iterable = iterable_class(content)
 
     ##
