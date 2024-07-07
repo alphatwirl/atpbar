835815f7e5077e12569c0789c233ade85416e1c1
Tai Sakuma <tai.sakuma@gmail.com>
2024-07-07 17:41:10 -0400

    Remove the `total` option from `Report`

--- atpbar/progress_report/report.py
+++ atpbar/progress_report/report.py
@@ -2,32 +2,24 @@ from typing import TypedDict
 from uuid import UUID
 
 
-class Report(TypedDict, total=False):
+class Report(TypedDict):
     '''Progress report
 
     Parameters
     ----------
-    task_id : immutable
+    task_id
         The unique task ID.
-    done : int
+    done
         The number of the iterations done so far
-    total : int
-            The total iterations to be done
-    name : str
-            A name of the task. It will be use as the label on the
-            progress bars.
+    total
+        The total iterations to be done
+    name
+        A name of the task. It will be use as the label on the progress bars.
     first : bool
-            `True` if the first report for the task. If not given,
-            automatically determined from `done`; `True` if `done` is
-            0, `False` otherwise
+       `True` if the first report for the task.
     last : bool
-            `True` if the last report for the task. If not given,
-            automatically determined from `done` and `total`; `True`
-            if `done` equals `total`, `False` otherwise
+       `True` if the last report for the task.
 
-    Notes:
-    ------
-    TODO: Use typing.Required for Python 3.11 and above
     '''
 
     task_id: UUID
