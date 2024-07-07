7a8dde584773e83e825ae5259f5f0a5b9474e7cf
Tai Sakuma <tai.sakuma@gmail.com>
2024-07-07 16:24:31 -0400

    Update tests

--- tests/progress_report/test_report_complement.py
+++ tests/progress_report/test_report_complement.py
@@ -1,136 +1,59 @@
-import uuid
-
-import pytest
+from hypothesis import given
+from hypothesis import strategies as st
 
 from atpbar.progress_report import Report
 from atpbar.progress_report.complement import ProgressReportComplementer
+from tests.test_utils.st import st_text
 
 
-@pytest.fixture()
-def obj() -> ProgressReportComplementer:
-    ret = ProgressReportComplementer()
-    return ret
+@given(data=st.data())
+def test_complement(data: st.DataObject) -> None:
+    obj = ProgressReportComplementer()
+
+    id_ = data.draw(st.uuids(version=4))
+    name = data.draw(st_text())
+    total = data.draw(st.integers(min_value=0, max_value=10))
+
+    report = Report(task_id=id_, name=name, done=0, total=total)
+    obj(report)
+    assert report['first']
+
+    for i in range(total):
+        done = i + 1
+        report = Report(task_id=id_, done=done, total=total)
+        obj(report)
+        assert not report['first']
+        assert report['last'] is (done == total)
+    else:
+        assert report['last']
 
 
-def test_repr(obj: ProgressReportComplementer) -> None:
-    repr(obj)
+@given(data=st.data())
+def test_first(data: st.DataObject) -> None:
+    obj = ProgressReportComplementer()
+
+    id_ = data.draw(st.uuids(version=4))
+    name = data.draw(st_text())
+    total = data.draw(st.integers(min_value=0, max_value=10))
+
+    first = data.draw(st.booleans())
+
+    report = Report(task_id=id_, name=name, done=0, total=total, first=first)
+    obj(report)
+    assert report['first'] is first
 
 
-def test_complement(obj: ProgressReportComplementer) -> None:
+@given(data=st.data())
+def test_last(data: st.DataObject) -> None:
+    obj = ProgressReportComplementer()
 
-    task_id = uuid.uuid4()
-    report0 = Report(task_id=task_id, done=0, total=10, name='task1')
-    expected0 = Report(
-        task_id=task_id, done=0, total=10, first=True, last=False, name='task1'
-    )
-    obj(report0)
-    assert expected0 == report0
+    id_ = data.draw(st.uuids(version=4))
+    name = data.draw(st_text())
+    total = data.draw(st.integers(min_value=0, max_value=10))
+    done = data.draw(st.integers(min_value=0, max_value=total))
 
-    report1 = Report(task_id=task_id, done=1, total=12)
-    expected1 = Report(
-        task_id=task_id, done=1, total=12, first=False, last=False, name='task1'
-    )
-    obj(report1)
-    assert expected0 == report0
-    assert expected1 == report1
+    last = data.draw(st.booleans())
 
-    report2 = Report(task_id=task_id, done=2)
-    expected2 = Report(
-        task_id=task_id, done=2, total=12, first=False, last=False, name='task1'
-    )
-    obj(report2)
-    assert expected0 == report0
-    assert expected1 == report1
-    assert expected2 == report2
-
-    report3 = Report(task_id=task_id, done=12)
-    expected3 = Report(
-        task_id=task_id, done=12, total=12, first=False, last=True, name='task1'
-    )
-    obj(report3)
-    assert expected0 == report0
-    assert expected1 == report1
-    assert expected2 == report2
-    assert expected3 == report3
-
-
-def test_volatile_fields(obj: ProgressReportComplementer) -> None:
-
-    task_id = uuid.uuid4()
-    report0 = Report(task_id=task_id, done=0, total=10, first=True)
-    expected0 = Report(task_id=task_id, done=0, total=10, first=True, last=False)
-    obj(report0)
-    assert expected0 == report0
-
-    # manually set `first`
-    report1 = Report(task_id=task_id, done=1, total=12, first=True, last=False)
-    expected1 = Report(task_id=task_id, done=1, total=12, first=True, last=False)
-    obj(report1)
-    assert expected0 == report0
-    assert expected1 == report1
-
-    # doesn't remember previously manually set `first`
-    report2 = Report(task_id=task_id, done=2)
-    expected2 = Report(task_id=task_id, done=2, total=12, first=False, last=False)
-    obj(report2)
-    assert expected0 == report0
-    assert expected1 == report1
-    assert expected2 == report2
-
-
-def test_first(obj: ProgressReportComplementer) -> None:
-
-    task_id_1 = uuid.uuid4()
-    report0 = Report(task_id=task_id_1, done=0, total=10)
-    expected0 = Report(task_id=task_id_1, done=0, total=10, first=True, last=False)
-    obj(report0)
-    assert expected0 == report0
-
-    report1 = Report(task_id=task_id_1, done=1, total=12)
-    expected1 = Report(task_id=task_id_1, done=1, total=12, first=False, last=False)
-    obj(report1)
-    assert expected1 == report1
-
-    report2 = Report(task_id=task_id_1, done=2, first=True)
-    expected2 = Report(task_id=task_id_1, done=2, total=12, first=True, last=False)
-    obj(report2)
-    assert expected2 == report2
-
-    report3 = Report(task_id=task_id_1, done=3)
-    expected3 = Report(task_id=task_id_1, done=3, total=12, first=False, last=False)
-    obj(report3)
-    assert expected3 == report3
-
-    # different task_id, both done and total are 0
-    task_id_2 = uuid.uuid4()
-    report4 = Report(task_id=task_id_2, done=0, total=0)
-    expected4 = Report(task_id=task_id_2, done=0, total=0, first=True, last=True)
-    obj(report4)
-    assert expected4 == report4
-
-
-def test_last(obj: ProgressReportComplementer) -> None:
-
-    task_id_1 = uuid.uuid4()
-    report0 = Report(task_id=task_id_1, done=0, total=10)
-    expected0 = Report(task_id=task_id_1, done=0, total=10, first=True, last=False)
-    obj(report0)
-    assert expected0 == report0
-
-    # set `last` manually
-    report1 = Report(task_id=task_id_1, done=1, total=12, last=True)
-    expected1 = Report(task_id=task_id_1, done=1, total=12, first=False, last=True)
-    obj(report1)
-    assert expected1 == report1
-
-    task_id_2 = uuid.uuid4()
-    report2 = Report(task_id=task_id_2, done=0, total=10)
-    expected2 = Report(task_id=task_id_2, done=0, total=10, first=True, last=False)
-    obj(report2)
-    assert expected2 == report2
-
-    # done = total
-    report3 = Report(task_id=task_id_2, done=10, total=10)
-    expected3 = Report(task_id=task_id_2, done=10, total=10, first=False, last=True)
-    obj(report3)
-    assert expected3 == report3
+    report = Report(task_id=id_, name=name, done=done, total=total, last=last)
+    obj(report)
+    assert report['last'] is last
