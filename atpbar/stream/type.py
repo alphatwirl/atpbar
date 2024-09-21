554dcac89aa12b2e7a0cf52302cf3b6c5260d9c0
Tai Sakuma <tai.sakuma@gmail.com>
2024-09-21 14:26:03 -0400

    Import `TypeAlias` from `type_extensions` in Python 3.9

--- atpbar/stream/type.py
+++ atpbar/stream/type.py
@@ -1,7 +1,11 @@
+import sys
 from enum import Enum
 from multiprocessing import Queue
-from typing import TypeAlias
 
+if sys.version_info >= (3, 10):
+    from typing import TypeAlias
+else:
+    from typing_extensions import TypeAlias
 
 class FD(Enum):
     STDOUT = 1
